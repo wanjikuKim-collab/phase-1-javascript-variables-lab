45d3b52ef92c99a3f3f368e2dbe1601f00c5e65e
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-21 12:03:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+// Using the const identifier to declare a variable 
+const companyName = 'Scuber';
+// Using the let identifier to declare a variable 
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
